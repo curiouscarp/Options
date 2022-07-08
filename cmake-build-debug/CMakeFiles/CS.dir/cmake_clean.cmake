63682b356d499b8ec9fb7fa60482a947631d6f4c
tapitennis <70279919+tapitennis@users.noreply.github.com>
2022-07-07 22:36:21 -0700

    Add files via upload

---
+++ cmake-build-debug/CMakeFiles/CS.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/CS.dir/main.cpp.o"
+  "CS"
+  "CS.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/CS.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
